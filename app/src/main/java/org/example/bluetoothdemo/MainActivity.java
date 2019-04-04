1070c6595bcbdd3e7fd7fd9f0dcb5b8d2b969a6c
x <x@x.com>
2019-04-04 17:06:10 +0800

    Implement the function of searching BT devices

--- app/src/main/java/org/example/bluetoothdemo/MainActivity.java
+++ app/src/main/java/org/example/bluetoothdemo/MainActivity.java
@@ -1,8 +1,15 @@
 package org.example.bluetoothdemo;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,5 +23,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
+        text2 = (TextView) findViewById(R.id.textView2);
+        text3 = (TextView) findViewById(R.id.textView3);
+        button = (Button) findViewById(R.id.button);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+        IntentFilter filter2 = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(mReceiver, filter2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!mBluetoothAdapter.isEnabled()) {
+                    mBluetoothAdapter.enable();
+                }
+                mBluetoothAdapter.startDiscovery();
+                text2.setText("Searching...");
+            }
+        });
     }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(mReceiver);
+        Log.e("destroy", "unregister");
+    }
+
+
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.e("ywq", action);
+
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+                if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                    text.append("\n" + device.getName() + "==>" + device.getAddress() + "\n");
+                } else {
+                    text3.append("\n" + device.getName() + "==>" + device.getAddress() + "\n");
+                }
+            } else if (action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+                text2.setText("Search finished.");
+            }
+        }
+    };
 }
