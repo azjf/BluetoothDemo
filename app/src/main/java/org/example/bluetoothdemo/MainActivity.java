5405755676d07938f11447210e52f0945e4542c4
x <x@x.com>
2019-04-04 16:33:49 +0800

    Create UI

--- app/src/main/java/org/example/bluetoothdemo/MainActivity.java
+++ app/src/main/java/org/example/bluetoothdemo/MainActivity.java
@@ -1,9 +1,16 @@
 package org.example.bluetoothdemo;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private TextView text, text2, text3;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
